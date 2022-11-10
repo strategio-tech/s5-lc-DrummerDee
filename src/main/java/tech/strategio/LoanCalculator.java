8d1ca66efaeaa436eef7771b07d814549730ca49
Drummer Dee <drummer.deja@gmail.com>
2022-11-10 10:13:42 -0500

    "Fixed the JavaDoc comment and put try block back"

--- src/main/java/tech/strategio/LoanCalculator.java
+++ src/main/java/tech/strategio/LoanCalculator.java
@@ -2,7 +2,11 @@ package tech.strategio;
 import java.util.Scanner;
 
 public class LoanCalculator {
-
+    /**
+     * Receives amount and iterate by the amount of months (3).
+     *  Method takes amount as the input.
+     * @return / Returns loan amount.
+     */
 
     static int getRemainingAmountIn3Months(int amount) {
         int months = 3;
@@ -14,9 +18,10 @@ public class LoanCalculator {
     }
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        try (Scanner scanner = new Scanner(System.in)) {
             int amount = scanner.nextInt();
             System.out.println(getRemainingAmountIn3Months(amount));
+        }
 
     }
 }
