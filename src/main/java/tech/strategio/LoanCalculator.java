15d84c2f88bfbee44ef18d9e595bddd415ce9c59
Drummer Dee <drummer.deja@gmail.com>
2022-11-10 10:05:16 -0500

    "First attempt"

--- src/main/java/tech/strategio/LoanCalculator.java
+++ src/main/java/tech/strategio/LoanCalculator.java
@@ -1,25 +1,22 @@
 package tech.strategio;
-
 import java.util.Scanner;
 
 public class LoanCalculator {
 
-    /**
-     * TODO: finish out this JavaDoc comment block.
-     * FIXME: WHAT DOES THIS METHOD DO?
-     * 
-     * @param ???
-     * @return ???
-     */
+
     static int getRemainingAmountIn3Months(int amount) {
-        // TODO: Rewrite this method
-        return -1;
+        int months = 3;
+        for(int x = 0; x < months; x++)
+            amount = amount * 90/100;
+        System.out.println(amount);
+
+        return amount;
     }
 
     public static void main(String[] args) {
-        try (Scanner scanner = new Scanner(System.in)) {
+        Scanner scanner = new Scanner(System.in);
             int amount = scanner.nextInt();
             System.out.println(getRemainingAmountIn3Months(amount));
-        }
+
     }
 }
